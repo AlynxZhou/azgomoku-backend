1b6357c508ea717d03cd74d0c8ec2cc6ef40aeb2
Alynx Zhou <alynx.zhou@gmail.com>
2019-07-15 15:25:53 +0800

    Replace tab with 2 spaces.

--- index.js
+++ index.js
@@ -9,230 +9,230 @@ let nextWid = 0
 let nextSid = 0
 
 const newWid = () => {
-	return `w${nextWid++}`
+  return `w${nextWid++}`
 }
 
 const newSid = () => {
-	return `s${nextSid++}`
+  return `s${nextSid++}`
 }
 
 const emitList = (socket, watcher) => {
-	socket.emit("list", JSON.stringify({
-		"sessions": Object.values(sessions).map((session) => {
-			return {
-				'sid': session.sid,
-				'wids': session.getWatchers(watchers).map((watcher) => {
-					return watcher.wid
-				}),
-				'canJoin': session.canJoin(watcher)
-			}
-		}),
-	}))
+  socket.emit("list", JSON.stringify({
+    "sessions": Object.values(sessions).map((session) => {
+      return {
+        'sid': session.sid,
+        'wids': session.getWatchers(watchers).map((watcher) => {
+          return watcher.wid
+        }),
+        'canJoin': session.canJoin(watcher)
+      }
+    }),
+  }))
 }
 
 const emitMap = (socket, session) => {
-	socket.emit('map', JSON.stringify({
-		'result': session.result,
-		'nextColor': session.nextColor,
-		'map': session['map'],
-		'mapx': session['mapx'],
-		'mapy': session['mapy']
-	}))
+  socket.emit('map', JSON.stringify({
+    'result': session.result,
+    'nextColor': session.nextColor,
+    'map': session['map'],
+    'mapx': session['mapx'],
+    'mapy': session['mapy']
+  }))
 }
 
 io.on('connection', (socket) => {
-	socket.on('watcher', (msg) => {
-		const watcher = new Watcher(newWid(), socket)
-		watchers[watcher.wid] = watcher
-		socket.wid = watcher.wid
-		socket.emit('watcher', JSON.stringify({'wid': watcher.wid}))
-	})
+  socket.on('watcher', (msg) => {
+    const watcher = new Watcher(newWid(), socket)
+    watchers[watcher.wid] = watcher
+    socket.wid = watcher.wid
+    socket.emit('watcher', JSON.stringify({'wid': watcher.wid}))
+  })
 
-	socket.on('list', (msg) => {
-		const data = JSON.parse(msg)
-		const {wid} = data
-		const watcher = watchers[wid]
-		emitList(socket, watcher)
-	})
+  socket.on('list', (msg) => {
+    const data = JSON.parse(msg)
+    const {wid} = data
+    const watcher = watchers[wid]
+    emitList(socket, watcher)
+  })
 
-	socket.on('session', (msg) => {
-		const session = new Session(newSid())
-		sessions[session.sid] = session
-		socket.emit("session", JSON.stringify({"sid": session.sid}))
-		Object.values(watchers).map((watcher) => {
-			emitList(watcher.socket, watcher)
-		})
-	})
+  socket.on('session', (msg) => {
+    const session = new Session(newSid())
+    sessions[session.sid] = session
+    socket.emit("session", JSON.stringify({"sid": session.sid}))
+    Object.values(watchers).map((watcher) => {
+      emitList(watcher.socket, watcher)
+    })
+  })
 
-	socket.on('yoroshiku', (msg) => {
-		const data = JSON.parse(msg)
-		let {sid, wid} = data
-		if (wid == null) {
-			return
-		}
-		const watcher = watchers[wid]
-		if (watcher == null) {
-			return
-		}
-		const session = sessions[sid]
-		if (session == null) {
-			socket.emit('arimasen')
-			return
-		}
-		if (!session.canJoin(watcher)) {
-			return
-		}
-		if (session.black !== watcher && session.white !== watcher) {
-			// First is black.
-			if (session.black == null) {
-				watcher.color = Color.BLACK
-				session.black = watcher
-			} else if (session.white == null) {
-				watcher.color = Color.WHITE
-				session.white = watcher
-			}
-		}
-		// Add watcher to session.
-		watcher.sid = session.sid
-		socket.emit('kochirakoso', JSON.stringify({
-			'sid': session.sid,
-			'color': watcher.color
-		}))
-		emitMap(watcher.socket, session)
-	})
+  socket.on('yoroshiku', (msg) => {
+    const data = JSON.parse(msg)
+    let {sid, wid} = data
+    if (wid == null) {
+      return
+    }
+    const watcher = watchers[wid]
+    if (watcher == null) {
+      return
+    }
+    const session = sessions[sid]
+    if (session == null) {
+      socket.emit('arimasen')
+      return
+    }
+    if (!session.canJoin(watcher)) {
+      return
+    }
+    if (session.black !== watcher && session.white !== watcher) {
+      // First is black.
+      if (session.black == null) {
+        watcher.color = Color.BLACK
+        session.black = watcher
+      } else if (session.white == null) {
+        watcher.color = Color.WHITE
+        session.white = watcher
+      }
+    }
+    // Add watcher to session.
+    watcher.sid = session.sid
+    socket.emit('kochirakoso', JSON.stringify({
+      'sid': session.sid,
+      'color': watcher.color
+    }))
+    emitMap(watcher.socket, session)
+  })
 
-	socket.on('sumimasen', (msg) => {
-		const data = JSON.parse(msg)
-		const {wid, sid} = data
-		if (wid == null) {
-			return
-		}
-		const watcher = watchers[wid]
-		if (watcher == null) {
-			return
-		}
-		const session = sessions[sid]
-		if (session == null) {
-			return
-		}
-		// Add watcher to session.
-		watcher.sid = session.sid
-		socket.emit('hai', JSON.stringify({
-			'sid': session.sid
-		}))
-		emitMap(watcher.socket, session)
-	})
+  socket.on('sumimasen', (msg) => {
+    const data = JSON.parse(msg)
+    const {wid, sid} = data
+    if (wid == null) {
+      return
+    }
+    const watcher = watchers[wid]
+    if (watcher == null) {
+      return
+    }
+    const session = sessions[sid]
+    if (session == null) {
+      return
+    }
+    // Add watcher to session.
+    watcher.sid = session.sid
+    socket.emit('hai', JSON.stringify({
+      'sid': session.sid
+    }))
+    emitMap(watcher.socket, session)
+  })
 
-	socket.on('step', (msg) => {
-		const data = JSON.parse(msg)
-		const {sid, wid, step} = data
-		const session = sessions[sid]
-		if (session == null) {
-			return
-		}
-		const watcher = watchers[wid]
-		const {color} = watcher
-		if (color === Color.NONE ||
-		    Object.values(Color).indexOf(color) === -1 ||
-		    color !== session.nextColor) {
-			return
-		}
-		const record = {'color': color, 'x': step.x, 'y': step.y}
-		if (session.map[session.index(record.x, record.y)] === Color.NONE) {
-			session.map[session.index(record.x, record.y)] = record.color
-		} else {
-			return
-		}
-		session.records.push(record)
-		session.checkMap()
-		if (color === Color.WHITE) {
-			session.nextColor = Color.BLACK
-		} else if (color === Color.BLACK) {
-			session.nextColor = Color.WHITE
-		}
-		session.getWatchers(watchers).map((watcher) => {
-			emitMap(watcher.socket, session)
-		})
-		if (session.result != null) {
-			session.getWatchers(watcher).map((watcher) => {
-				watcher.sid = null
-			})
-			delete sessions[session.sid]
-			Object.values(watchers).map((watcher) => {
-				emitList(watcher.socket)
-			})
-		}
-	})
+  socket.on('step', (msg) => {
+    const data = JSON.parse(msg)
+    const {sid, wid, step} = data
+    const session = sessions[sid]
+    if (session == null) {
+      return
+    }
+    const watcher = watchers[wid]
+    const {color} = watcher
+    if (color === Color.NONE ||
+        Object.values(Color).indexOf(color) === -1 ||
+        color !== session.nextColor) {
+      return
+    }
+    const record = {'color': color, 'x': step.x, 'y': step.y}
+    if (session.map[session.index(record.x, record.y)] === Color.NONE) {
+      session.map[session.index(record.x, record.y)] = record.color
+    } else {
+      return
+    }
+    session.records.push(record)
+    session.checkMap()
+    if (color === Color.WHITE) {
+      session.nextColor = Color.BLACK
+    } else if (color === Color.BLACK) {
+      session.nextColor = Color.WHITE
+    }
+    session.getWatchers(watchers).map((watcher) => {
+      emitMap(watcher.socket, session)
+    })
+    if (session.result != null) {
+      session.getWatchers(watcher).map((watcher) => {
+        watcher.sid = null
+      })
+      delete sessions[session.sid]
+      Object.values(watchers).map((watcher) => {
+        emitList(watcher.socket)
+      })
+    }
+  })
 
-	socket.on('sayonara', (msg) => {
-		const data = JSON.parse(msg)
-		const {wid} = data
-		const watcher = watchers[wid]
-		const session = sessions[watcher.sid]
-		delete watchers[watcher.wid]
-		if (session == null) {
-			return
-		}
-		if (session.black === watcher) {
-			session.getWatchers(watchers).map((watcher) => {
-				watcher.socket.emit('sayonara', JSON.stringify({
-					'sayonara': Color.BLACK
-				}))
-			})
-			delete sessions[session.sid]
-			Object.values(watchers).map((watcher) => {
-				emitList(watcher.socket)
-			})
-		}
-		if (session.white === watcher) {
-			session.getWatchers(watchers).map((watcher) => {
-				watcher.socket.emit('sayonara', JSON.stringify({
-					'sayonara': Color.WHITE
-				}))
-			})
-			delete sessions[session.sid]
-			Object.values(watchers).map((watcher) => {
-				emitList(watcher.socket)
-			})
-		}
-	})
+  socket.on('sayonara', (msg) => {
+    const data = JSON.parse(msg)
+    const {wid} = data
+    const watcher = watchers[wid]
+    const session = sessions[watcher.sid]
+    delete watchers[watcher.wid]
+    if (session == null) {
+      return
+    }
+    if (session.black === watcher) {
+      session.getWatchers(watchers).map((watcher) => {
+        watcher.socket.emit('sayonara', JSON.stringify({
+          'sayonara': Color.BLACK
+        }))
+      })
+      delete sessions[session.sid]
+      Object.values(watchers).map((watcher) => {
+        emitList(watcher.socket)
+      })
+    }
+    if (session.white === watcher) {
+      session.getWatchers(watchers).map((watcher) => {
+        watcher.socket.emit('sayonara', JSON.stringify({
+          'sayonara': Color.WHITE
+        }))
+      })
+      delete sessions[session.sid]
+      Object.values(watchers).map((watcher) => {
+        emitList(watcher.socket)
+      })
+    }
+  })
 
-	socket.on('disconnect', () => {
-		if (socket.wid == null) {
-			return
-		}
-		const watcher = watchers[socket.wid]
-		delete watcher[socket.wid]
-		if (watcher == null) {
-			return
-		}
-		const session = sessions[watcher.sid]
-		if (session == null) {
-			return
-		}
-		if (session.black === watcher) {
-			session.getWatchers(watchers).map((watcher) => {
-				watcher.socket.emit('sayonara', JSON.stringify({
-					'sayonara': Color.BLACK
-				}))
-			})
-			delete sessions[session.sid]
-			Object.values(watchers).map((watcher) => {
-				emitList(watcher.socket)
-			})
-		}
-		if (session.white === watcher) {
-			session.getWatchers(watchers).map((watcher) => {
-				watcher.socket.emit('sayonara', JSON.stringify({
-					'sayonara': Color.WHITE
-				}))
-			})
-			delete sessions[session.sid]
-			Object.values(watchers).map((watcher) => {
-				emitList(watcher.socket)
-			})
-		}
-	})
+  socket.on('disconnect', () => {
+    if (socket.wid == null) {
+      return
+    }
+    const watcher = watchers[socket.wid]
+    delete watcher[socket.wid]
+    if (watcher == null) {
+      return
+    }
+    const session = sessions[watcher.sid]
+    if (session == null) {
+      return
+    }
+    if (session.black === watcher) {
+      session.getWatchers(watchers).map((watcher) => {
+        watcher.socket.emit('sayonara', JSON.stringify({
+          'sayonara': Color.BLACK
+        }))
+      })
+      delete sessions[session.sid]
+      Object.values(watchers).map((watcher) => {
+        emitList(watcher.socket)
+      })
+    }
+    if (session.white === watcher) {
+      session.getWatchers(watchers).map((watcher) => {
+        watcher.socket.emit('sayonara', JSON.stringify({
+          'sayonara': Color.WHITE
+        }))
+      })
+      delete sessions[session.sid]
+      Object.values(watchers).map((watcher) => {
+        emitList(watcher.socket)
+      })
+    }
+  })
 })
 
 io.listen(3003)
--- types.js
+++ types.js
@@ -1,164 +1,164 @@
 const Color = {NONE: 1, WHITE: 2, BLACK: 3}
 
 class Session {
-	constructor(sid) {
-		this.sid = sid
-		this.win = 5
-		this.mapx = 15
-		this.mapy = 15
-		this.map = new Array(this.mapx * this.mapy).fill(Color.NONE)
-		// Create cross lines to check.
-		// lines1 is from top-left to bottom-right.
-		this.lines1 = []
-		for (let i = 0; i <= this.mapy - this.win; ++i) {
-			this.lines1.push({
-				'begin': {'x': 0, 'y': i},
-				'end': {'x': this.mapy - 1 - i, 'y': this.mapy - 1}
-			})
-		}
-		for (let i = 0; i <= this.mapx - this.win; ++i) {
-			this.lines1.push({
-				'begin': {'x': i, 'y': 0},
-				'end': {'x': this.mapx - 1, 'y': this.mapx - 1 - i}
-			})
-		}
-		// lines2 is from bottom-left to top-right.
-		this.lines2 = []
-		for (let i = this.mapx - 1; i >= this.win - 1; --i) {
-			this.lines2.push({
-				'begin': {'x': 0, 'y': i},
-				'end': {'x': i, 'y': 0}
-			})
-		}
-		for (let i = 0; i <= this.mapy - this.win; ++i) {
-			this.lines2.push({
-				'begin': {'x': i, 'y': this.mapy - 1},
-				'end': {'x': this.mapx - 1, 'y': i}
-			})
-		}
-		this.white = null
-		this.black = null
-		this.nextColor = Color.BLACK
-		this.result = null
-		this.records = []
-	}
-	getWatchers(watchers) {
-		return Object.values(watchers).filter((watcher) => {
-			return watcher.sid === this.sid
-		})
-	}
-	canJoin(watcher) {
-		return watcher != null &&
-		       (this.black === watcher || this.white === watcher ||
-					  this.black == null || this.white == null)
-	}
-	index(x, y) {
-		return y * this.mapx + x
-	}
-	checkColumn() {
-		for (let i = 0; i < this.mapx; ++i) {
-			let gomoku = 0
-			let lastColor = Color.NONE
-			for (let j = 0; j < this.mapy; ++j) {
-				if (this.map[this.index(i, j)] !== Color.NONE) {
-					if (this.map[this.index(i, j)] === lastColor) {
-						if (++gomoku >= this.win) {
-							return lastColor
-						}
-					} else {
-						lastColor = this.map[this.index(i, j)]
-						gomoku = lastColor === Color.NONE ? 0 : 1
-					}
-				}
-			}
-		}
-		return null
-	}
-	checkRow() {
-		for (let j = 0; j < this.mapy; ++j) {
-			let gomoku = 0
-			let lastColor = Color.NONE
-			for (let i = 0; i < this.mapx; ++i) {
-				if (this.map[this.index(i, j)] !== Color.NONE) {
-					if (this.map[this.index(i, j)] === lastColor) {
-						if (++gomoku >= this.win) {
-							return lastColor
-						}
-					} else {
-						lastColor = this.map[this.index(i, j)]
-						gomoku = lastColor === Color.NONE ? 0 : 1
-					}
-				}
-			}
-		}
-		return null
-	}
-	checkCross() {
-		for (let line of this.lines1) {
-			let gomoku = 0
-			let lastColor = Color.NONE
-			for (let i = line.begin.x, j = line.begin.y;
-					 i <= line.end.x && j <= line.end.y;
-					 ++i, ++j) {
-				if (this.map[this.index(i, j)] !== Color.NONE) {
- 					if (this.map[this.index(i, j)] === lastColor) {
- 						if (++gomoku >= this.win) {
- 							return lastColor
- 						}
- 					} else {
-						lastColor = this.map[this.index(i, j)]
-						gomoku = lastColor === Color.NONE ? 0 : 1
- 					}
- 				}
-			}
-		}
-		for (let line of this.lines2) {
-			let gomoku = 0
-			let lastColor = Color.NONE
-			for (let i = line.begin.x, j = line.begin.y;
-					 i <= line.end.x && j >= line.end.y;
-					 ++i, --j) {
-				if (this.map[this.index(i, j)] !== Color.NONE) {
- 					if (this.map[this.index(i, j)] === lastColor) {
- 						if (++gomoku >= this.win) {
- 							return lastColor
- 						}
- 					} else {
- 						lastColor = this.map[this.index(i, j)]
-						gomoku = lastColor === Color.NONE ? 0 : 1
- 					}
- 				}
-			}
-		}
-		return null
-	}
-	checkMap() {
-		this.result = this.checkRow()
-		if (this.result != null) {
-			return
-		}
-		this.result = this.checkColumn()
-		if (this.result != null) {
-			return
-		}
-		this.result = this.checkCross()
-		if (this.result != null) {
-			return
-		}
-	}
+  constructor(sid) {
+    this.sid = sid
+    this.win = 5
+    this.mapx = 15
+    this.mapy = 15
+    this.map = new Array(this.mapx * this.mapy).fill(Color.NONE)
+    // Create cross lines to check.
+    // lines1 is from top-left to bottom-right.
+    this.lines1 = []
+    for (let i = 0; i <= this.mapy - this.win; ++i) {
+      this.lines1.push({
+        'begin': {'x': 0, 'y': i},
+        'end': {'x': this.mapy - 1 - i, 'y': this.mapy - 1}
+      })
+    }
+    for (let i = 0; i <= this.mapx - this.win; ++i) {
+      this.lines1.push({
+        'begin': {'x': i, 'y': 0},
+        'end': {'x': this.mapx - 1, 'y': this.mapx - 1 - i}
+      })
+    }
+    // lines2 is from bottom-left to top-right.
+    this.lines2 = []
+    for (let i = this.mapx - 1; i >= this.win - 1; --i) {
+      this.lines2.push({
+        'begin': {'x': 0, 'y': i},
+        'end': {'x': i, 'y': 0}
+      })
+    }
+    for (let i = 0; i <= this.mapy - this.win; ++i) {
+      this.lines2.push({
+        'begin': {'x': i, 'y': this.mapy - 1},
+        'end': {'x': this.mapx - 1, 'y': i}
+      })
+    }
+    this.white = null
+    this.black = null
+    this.nextColor = Color.BLACK
+    this.result = null
+    this.records = []
+  }
+  getWatchers(watchers) {
+    return Object.values(watchers).filter((watcher) => {
+      return watcher.sid === this.sid
+    })
+  }
+  canJoin(watcher) {
+    return watcher != null &&
+           (this.black === watcher || this.white === watcher ||
+            this.black == null || this.white == null)
+  }
+  index(x, y) {
+    return y * this.mapx + x
+  }
+  checkColumn() {
+    for (let i = 0; i < this.mapx; ++i) {
+      let gomoku = 0
+      let lastColor = Color.NONE
+      for (let j = 0; j < this.mapy; ++j) {
+        if (this.map[this.index(i, j)] !== Color.NONE) {
+          if (this.map[this.index(i, j)] === lastColor) {
+            if (++gomoku >= this.win) {
+              return lastColor
+            }
+          } else {
+            lastColor = this.map[this.index(i, j)]
+            gomoku = lastColor === Color.NONE ? 0 : 1
+          }
+        }
+      }
+    }
+    return null
+  }
+  checkRow() {
+    for (let j = 0; j < this.mapy; ++j) {
+      let gomoku = 0
+      let lastColor = Color.NONE
+      for (let i = 0; i < this.mapx; ++i) {
+        if (this.map[this.index(i, j)] !== Color.NONE) {
+          if (this.map[this.index(i, j)] === lastColor) {
+            if (++gomoku >= this.win) {
+              return lastColor
+            }
+          } else {
+            lastColor = this.map[this.index(i, j)]
+            gomoku = lastColor === Color.NONE ? 0 : 1
+          }
+        }
+      }
+    }
+    return null
+  }
+  checkCross() {
+    for (let line of this.lines1) {
+      let gomoku = 0
+      let lastColor = Color.NONE
+      for (let i = line.begin.x, j = line.begin.y;
+           i <= line.end.x && j <= line.end.y;
+           ++i, ++j) {
+        if (this.map[this.index(i, j)] !== Color.NONE) {
+           if (this.map[this.index(i, j)] === lastColor) {
+             if (++gomoku >= this.win) {
+               return lastColor
+             }
+           } else {
+            lastColor = this.map[this.index(i, j)]
+            gomoku = lastColor === Color.NONE ? 0 : 1
+           }
+         }
+      }
+    }
+    for (let line of this.lines2) {
+      let gomoku = 0
+      let lastColor = Color.NONE
+      for (let i = line.begin.x, j = line.begin.y;
+           i <= line.end.x && j >= line.end.y;
+           ++i, --j) {
+        if (this.map[this.index(i, j)] !== Color.NONE) {
+           if (this.map[this.index(i, j)] === lastColor) {
+             if (++gomoku >= this.win) {
+               return lastColor
+             }
+           } else {
+             lastColor = this.map[this.index(i, j)]
+            gomoku = lastColor === Color.NONE ? 0 : 1
+           }
+         }
+      }
+    }
+    return null
+  }
+  checkMap() {
+    this.result = this.checkRow()
+    if (this.result != null) {
+      return
+    }
+    this.result = this.checkColumn()
+    if (this.result != null) {
+      return
+    }
+    this.result = this.checkCross()
+    if (this.result != null) {
+      return
+    }
+  }
 }
 
 class Watcher {
-	constructor(wid, socket, color = Color.NONE) {
-		this.wid = wid
-		this.socket = socket
-		this.color = color
-		this.sid = null
-	}
+  constructor(wid, socket, color = Color.NONE) {
+    this.wid = wid
+    this.socket = socket
+    this.color = color
+    this.sid = null
+  }
 }
 
 module.exports = {
-	Color,
-	Session,
-	Watcher
+  Color,
+  Session,
+  Watcher
 }
